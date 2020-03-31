b49567443b07501bc028d05f15579a0a847051e9
Kazushi Konosu <kazushi@konosu.me>
2020-04-01 05:36:02 +0900

    fix

--- main.py
+++ main.py
@@ -15,33 +15,33 @@ month = 4
 day_before = 0
 
 while (True):
-	url = f"https://www.53cal.jp/areacalendar?city=1080104&area=1080104102&yy={year}&mm={month}"
-	html = urllib.request.urlopen(url)
-	soup = BeautifulSoup(html, "html.parser")
-	cells = soup.select('td')
-	for cell in cells:
-		if len(cell.select('#cal_kind')) == 0:
-			continue
-		src = cell.select('img')[0]['src'].strip()
-		day = int(re.sub(regex, r'\1', src))
+    url = f"https://www.53cal.jp/areacalendar?city=1080104&area=1080104102&yy={year}&mm={month}"
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html, "html.parser")
+    cells = soup.select('td')
+    for cell in cells:
+        if len(cell.select('#cal_kind')) == 0:
+            continue
+        src = cell.select('img')[0]['src'].strip()
+        day = int(re.sub(regex, r'\1', src))
 
-		if (day_before == day):
-			continue
-		else:
-			day_before = day
+        if (day_before == day):
+            continue
+        else:
+            day_before = day
 
-		e = Event()
-		e.name = cell.select('a')[0]['title'].replace('\n', ' ').strip()
-		e.begin = (datetime(year, month, day, 8, 0), 'Asia/Tokyo')
-		e.end = e.begin
-		c.events.add(e)
-	if month == 3 and year == 2020:
-		break
-	if month == 12:
-		year += 1
-		month = 1
-	else:
-		month += 1
+        e = Event()
+        e.name = cell.select('a')[0]['title'].replace('\n', ' ').strip()
+        e.begin = (datetime(year, month, day, 8, 0), 'Asia/Tokyo')
+        e.end = e.begin
+        c.events.add(e)
+    if month == 3 and year == 2020:
+        break
+    if month == 12:
+        year += 1
+        month = 1
+    else:
+        month += 1
 
 with open('waste.ics', 'w') as waste_file:
-	waste_file.writelines(c)
+    waste_file.writelines(c)
